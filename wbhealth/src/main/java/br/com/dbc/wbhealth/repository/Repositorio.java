a2231e285329d659a6710f2989c08c5043b20976
RenanBilhan <renan.bilhan@hotmail.com>
2023-08-04 16:42:54 -0300

    Fix Interface Repositorio

--- wbhealth/src/main/java/br/com/dbc/wbhealth/repository/Repositorio.java
+++ wbhealth/src/main/java/br/com/dbc/wbhealth/repository/Repositorio.java
@@ -8,17 +8,17 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface Repositorio<Key, T> {
-    public void cadastrar(T entidade) throws BancoDeDadosException;
+    public T save(T entidade) throws BancoDeDadosException;
 
-    public List<T> listarTodos() throws BancoDeDadosException;
+    public List<T> findALL() throws BancoDeDadosException;
 
-    public T listarPeloId(Key id) throws BancoDeDadosException;
+    public T findById(Key id) throws BancoDeDadosException;
 
-    public boolean alterarPeloId(Key id, T entidadeAtualizada) throws BancoDeDadosException;
+    public T update(Key id, T entidadeAtualizada) throws BancoDeDadosException;
 
-    public boolean deletarPeloId(Key id) throws BancoDeDadosException;
+    public boolean deleteById(Key id) throws BancoDeDadosException;
 
     public Integer getProximoId(Connection connection, String nextSequence) throws SQLException;
-    public T buscarId(Key id) throws BancoDeDadosException;
+//    public T buscarId(Key id) throws BancoDeDadosException;
 
 }
